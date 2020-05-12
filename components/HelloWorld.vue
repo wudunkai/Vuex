bbf37b040b5203887abbe9d68f8992c772101c21
wdk15070234551@163.com <wdk15070234551@163.com>
2020-05-12 16:48:19 +0800

    Vuex状态管理

---
+++ components/HelloWorld.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="hello">
+    <h3>{{$store.state.count}}</h3>
+    <div>
+      <button @click="handleAddClick(10)">增加</button>
+      <button @click="handleReduceClick(10)">减少</button>
+    </div>
+    <div>异步操作</div>
+    <div>
+      <button @click="handleActionsAdd(10)">异步增加</button>
+      <button @click="handleActionsReduce(10)">异步减少</button>
+    </div>
+    <h4>{{count}}</h4>
+  </div>
+</template>
+<script>
+import {mapState, mapMutations, mapActions, mapGetters} from 'vuex'
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    }
+  },
+  methods:{
+    ...mapMutations({
+      handleAddClick: 'mutationsAddCount',
+      handleReduceClick: 'mutationsReduceCount'
+    }),
+    ...mapActions({
+      handleActionsAdd: 'actionsAddCount',
+      handleActionsReduce: 'actionsReduceCount'
+    })
+    // // mutations
+    // handleAddClick(n){
+    //   this.$store.commit('mutationsAddCount',n);
+    // },
+    // handleReduceClick(n){
+    //   this.$store.commit('mutationsReduceCount',n);
+    // },
+    // // actions
+    // handleActionsAdd(n){
+    //   this.$store.dispatch('actionsAddCount',n)
+    // },
+    // handleActionsReduce(n){
+    //   this.$store.dispatch('actionsReduceCount',n)
+    // },
+  },
+  computed:{
+    count(){
+      // return this.$store.getters.getterCount 
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
